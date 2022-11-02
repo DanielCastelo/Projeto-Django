3c16166355ddda218423647c869d17670bb52a88
DanielCastelo <danieltolezani@outlook.com>
2022-11-02 18:04:26 -0300

    Templates

--- recipes/views.py
+++ recipes/views.py
@@ -3,13 +3,15 @@ from django.shortcuts import render
 
 
 def Home(request):
-    return HttpResponse('Teste Home')
+    return render(request, 'recipes/home.html')
+
+def Contato(request):
+    return render(request, 'recipes/contato.html')
 
 def Sobre(request):
     return HttpResponse('Teste Sobre')
 
-def Contato(request):
-    return HttpResponse('Teste Contato')
+
 
 
 # Create your views here.
