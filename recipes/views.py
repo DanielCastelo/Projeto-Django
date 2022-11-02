d6505122bf8b8fa0f6eaa2d376f4640c345e97b3
DanielCastelo <danieltolezani@outlook.com>
2022-11-02 18:13:18 -0300

    Remoção das abas "Contato" e "Sobre"

--- recipes/views.py
+++ recipes/views.py
@@ -1,17 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def Home(request):
-    return render(request, 'recipes/home.html')
-
-def Contato(request):
-    return render(request, 'recipes/contato.html')
-
-def Sobre(request):
-    return HttpResponse('Teste Sobre')
-
-
-
+    return render(request, 'recipes/home.html',context={'name': 'Daniel Tolezani Castelo'}),
 
 # Create your views here.
